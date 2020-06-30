e1dafee477fa568795ae312dcf1c142afb82edf1
atikur-rabbi <msawon94@gmail.com>
2020-07-01 02:11:49 +0600

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:todo/ui/todo_screen.dart';
+
+void main() {
+  runApp(TodoApp());
+}
+
+
+class TodoApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: TodoScreen(),
+    );
+  }
+}
+
